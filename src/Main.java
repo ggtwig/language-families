3cdc0c1bb944937124ecb48f2ad772e94c72d70d
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-08-23 17:51:57 +0100

    updated main class functionality

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Language spanish = new Language("Spanish", 555000000, "Spain, Latin America, and Equatorial Guinea", "subject-verb-object");
+        Language spanish = new IndoEuropean("Spanish", 555000000);
         Language kiche = new Mayan("Ki'che", 2330000);
         Language mandarin = new SinoTibetan("Mandarin Chinese", 1110000000);
         Language burmese = new SinoTibetan("Burmese", 43000000);
