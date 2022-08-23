d474eca79e7918f77f43da01b651181ab6f3448a
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-08-23 17:52:27 +0100

    added IndoEuropean class + updated constructors

---
+++ src/IndoEuropean.java
@@ -0,0 +1,6 @@
+public class IndoEuropean extends Language {
+
+    IndoEuropean(String name, int numSpeakers) {
+        super(name, numSpeakers, "Spain, Latin America, and Equatorial Guinea", "subject-verb-object");
+    }
+}
